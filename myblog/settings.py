977703e6eff3fbc84de2b981447803b155098ccf
kaysam-clone <65505237+kaysam-clone@users.noreply.github.com>
2023-03-20 12:06:04 -0700

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -135,13 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/home/bloggar/myblog/static"
-# or, eg,
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
 LOGIN_REDIRECT_URL = '/'
 
 LOGIN_URL = '/login'
